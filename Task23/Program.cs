f0cde78df88ab64008f39e1003264a6e5f08d512
boslash15 <boslash15@gmail.com>
2022-10-09 23:06:57 +0300

    записала решение 23 задачи

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -7,3 +7,19 @@
 // 3 | 27
 // 4 | 64
 // 5 | 125
+
+void CubeTable(int num)
+{
+    int index = 1;
+    Console.WriteLine($"Таблица кубов чисел от 1 до {num}");
+    while (index <= num)
+    {
+        Console.WriteLine($"|{index,4}|{Math.Pow(index, 3),4}|");
+        index++;
+    }
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubeTable(number);
