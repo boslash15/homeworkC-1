5fafed0c46c3664612b35df3108b03dd2eeee970
boslash15 <boslash15@gmail.com>
2022-10-09 21:11:53 +0300

    Записала решение 15 задачи

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,3 +3,21 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите цифру, обозначающую день недели, для определения выходного дня: ");
+int dayweek = Convert.ToInt32(Console.ReadLine());
+
+if (dayweek < 0) Console.WriteLine("некорректно введен день недели");
+else if (dayweek > 7) Console.WriteLine("некорректно введен день недели");
+
+bool dayOff = DayOff(dayweek);
+if (dayOff) Console.WriteLine("да");
+else Console.WriteLine("нет");
+
+
+bool DayOff(int dweek)
+{
+    if (dweek == 6) return true;
+    if (dweek == 7) return true;
+    return false;
+}
