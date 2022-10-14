b1b7477a34c6f630a5526b6233ccc5bfc63b79b2
boslash15 <boslash15@gmail.com>
2022-10-14 19:23:35 +0300

    Записала условие 25 задачи

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿// Задача 25: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
 double numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 double numberB = Convert.ToInt32(Console.ReadLine());
