c339a060f38217876086f3f4260c4fcec48eb00e
boslash15 <boslash15@gmail.com>
2022-10-14 19:13:19 +0300

    Записала решение 25 задачи

---
+++ Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите первое число: ");
+double numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double numberB = Convert.ToInt32(Console.ReadLine());
+
+double degreeAinB = DegreeNumber(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {degreeAinB}");
+
+double DegreeNumber(double numA, double numB)
+{
+    double AinB = 1;
+    if (numB > 0)
+        {    
+        while (numB > 0)
+            {
+            numB = numB - 1;
+            AinB = AinB * numA;
+            }
+        }    
+    else 
+        if (numB < 0)
+            {
+            while (numB < 0)
+                {
+                numB = numB + 1;
+                AinB = AinB / numA;
+                }
+            }
+        else
+            AinB = 1;   
+    return AinB;
+}
