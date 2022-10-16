55cf7ddbbf55852e77b8d55b1768cddb26b1bd14
boslash15 <boslash15@gmail.com>
2022-10-16 22:35:01 +0300

    Записала решение 29 задачи

---
+++ Task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+//  из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] array = {1, 2, 5, 7, 19, 6, 1, 33};
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+
+PrintArray(array);
