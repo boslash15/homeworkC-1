875ab80d6fae371c548f718b7390f5a14eeaab55
boslash15 <boslash15@gmail.com>
2022-11-10 20:47:48 +0300

    записала решение задачи 56

---
+++ Task56/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 56: 
+// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей 
+// суммой элементов: 1 строка
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)  // создаем массив
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)    // печатаем массив
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SumMatrix(int[,] matrix) // находим суммы строк массива
+{
+    int[] sumMatrix = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        sumMatrix[i] = sum;
+    }
+    int minSum = sumMatrix[0];
+    int row = 0;
+    for (int i = 1; i < sumMatrix.Length; i++)
+    {
+        if (minSum > sumMatrix[i])
+        {
+            minSum = sumMatrix[i];
+            row = i;
+        }
+    }
+    Console.WriteLine($"Найменьшая сумма {minSum}. Номер строки с наименьшей суммой элементов: {row + 1} строка");
+}
+
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+SumMatrix(array2D);
