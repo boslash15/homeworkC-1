43cec6aac8573e28a7229de5b9975a7baf02ee3b
boslash15 <boslash15@gmail.com>
2022-11-28 23:03:37 +0300

    Исправила решение 58 задачи

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -6,3 +6,66 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)  // создаем массив
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)    // печатаем массив
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] MatrixProduct(int[,] matrix1, int[,] matrix2)    // считаем произведение 2 матриц
+{
+    int[,] matrixProduct = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    if (matrix1.GetLength(0) == matrix2.GetLength(1))
+    {
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int l = 0; l < matrix1.GetLength(0); l++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    matrixProduct[i, l] += matrix1[i, j] * matrix2[j, l];
+                }
+            }
+        }
+    }
+    return matrixProduct;
+}
+
+int[,] array2A = CreateMatrixRndInt(3, 3, 1, 10);
+int[,] array2B = CreateMatrixRndInt(3, 3, 1, 10);
+PrintMatrix(array2A);
+Console.WriteLine();
+PrintMatrix(array2B);
+
+if (array2A.GetLength(0) == array2B.GetLength(1))
+{
+    int[,] array2AB = MatrixProduct(array2A, array2B);
+    Console.WriteLine();
+    PrintMatrix(array2AB);
+}
+else Console.WriteLine("Перемножить матрицы невозможно");
+
--- Task60/Program.cs
+++ Task60/Program.cs
@@ -6,3 +6,45 @@
 // Результат:
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt(int rows, int columns, int cell)  // создаем массив
+{
+    var matrix = new int[rows, columns, cell];
+    for (int f = 10; f < 100;)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(2); k++)
+                {
+                    matrix[i, j, k] = f;
+                    f = f + 1;
+                }
+            }
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)    // печатаем массив
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (k < matrix.GetLength(2) - 1) Console.Write($"{matrix[i, j, k]},({i},{j},{k}),     ");
+                else Console.Write($"{matrix[i, j, k]},({i},{j},{k})     ");
+            }
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+int[,,] array3 = CreateMatrixRndInt(2, 2, 2);
+PrintMatrix(array3);
