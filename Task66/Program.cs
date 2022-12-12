38559636305f31f93d4027322c528ef4db4de6e5
boslash15 <boslash15@gmail.com>
2022-12-12 19:34:02 +0300

    Решение 66 и 68 задачи

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("введите целое первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите целое второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int SumNaturalNumbers(int num1, int num2)
+{
+    if (num1 == num2) return num1;
+    if (num1 < num2) return num1 + SumNaturalNumbers(num1 + 1, num2);
+    if (num1 > num2) return num2 + SumNaturalNumbers(num1 - 1, num2);
+}
+
+int sum = SumNaturalNumbers(number1, number2);
+Console.WriteLine($"{sum}");
---
+++ Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью 
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.WriteLine("Введите первое неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+int FunkAkkerman(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    if (num2 == 0) return FunkAkkerman(num1 - 1, 1);
+    return FunkAkkerman(num1 - 1, FunkAkkerman(num1, num2 - 1));
+}
+
+int funkAkk = FunkAkkerman(n, m);
+Console.WriteLine($"{funkAkk}");
